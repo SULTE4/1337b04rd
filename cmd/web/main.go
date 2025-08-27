9ab24ff7fad4d0362fe272a4d2e84bf08a173d5b
suzhumaga <sult.zhumagaliev07@gmail.com>
2025-08-27 21:10:43 +0500

    feat: flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"1337b04rd/internal/app"
 	"flag"
+	"fmt"
 	"log"
 	"log/slog"
 	"net/http"
@@ -20,12 +21,34 @@ import (
 */
 
 func main() {
-	addr := flag.String("port", ":8080", "server address")
+	addr := flag.String("port", "8080", "Port number.")
+	help := flag.Bool("help", false, "Show this screen.")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "hacker board\n\n")
+		fmt.Fprintf(os.Stderr, "Usage:\n")
+		fmt.Fprintf(os.Stderr, "  1337b04rd [--port <N>]\n")
+		fmt.Fprintf(os.Stderr, "  1337b04rd --help\n\n")
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
+
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	*addr = ":" + *addr
 
 	dsn := "postgres://postgres:pass@localhost:5432/mydb?sslmode=disable"
 
-	app, err := app.NewApplication(dsn)
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	}))
+
+	app, err := app.NewApplication(dsn, *logger)
 	if err != nil {
 		log.Fatalf("failed to start app: %v", err)
 	}
